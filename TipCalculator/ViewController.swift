3090266cf660d234b75b43c0c9304e5bee905f1d
Ilana Mohamed <Imohamed@lcs.on.ca>
2019-10-10 12:05:30 -0400

    View completed and started on calculate tip code

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var tipPercentage: UITextField!
+    @IBOutlet weak var numberOfPeople: UITextField!
+    @IBOutlet weak var totalTip: UILabel!
+    @IBOutlet weak var tipPerPerson: UILabel!
+    @IBOutlet weak var billAmount: UITextField!
+    
+    
+    //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
 }
 
