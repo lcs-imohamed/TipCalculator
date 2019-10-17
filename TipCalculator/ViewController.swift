f4df3af1c537e306e888c5f2b091985abe66827a
Ilana Mohamed <Imohamed@lcs.on.ca>
2019-10-17 16:53:46 -0400

    Finished the tip calculation code and finished tip per person code. Finished!

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -16,31 +16,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipPerPerson: UILabel!
     @IBOutlet weak var billAmount: UITextField!
     
-   
-
+    
+    
     
     //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        // Obtain value from guess field
-        
-       let billAmount.text = billAmount
         
         
-        let billAmountAString = billAmount!
-        let tipPercentAString = tipPercentage!
-        let amountOfPeopleAString = numberOfPeople!
+        
         
     }
     
     
     @IBAction func calculateTip(_ sender: Any) {
-      
+        
+        // Obtain value from guess field
+        let billAmountAsString = billAmount.text!
+        let tipPercentAsString = tipPercentage.text!
+        let amountOfPeopleAsString = numberOfPeople.text!
+        
+        //Convert strings to double data types
+        let billAmountAsDouble = Double(billAmountAsString)!
+        let tipPercentAsDouble = Double(tipPercentAsString)!
+        let amountOfPeopleAsDouble = Double(amountOfPeopleAsString)!
+        
+        //Calculate the total tip
+        let tipAmountDollars = billAmountAsDouble * tipPercentAsDouble
         
         
-      
+        //Show the total tip
+        totalTip.text = String(tipAmountDollars)
         
+        //Calculate the tip per person
+        let tipPerPersonDollars = tipAmountDollars / amountOfPeopleAsDouble
+
+        //Show the tip per person
+        tipPerPerson.text = String(tipPerPersonDollars)
     }
     
     
